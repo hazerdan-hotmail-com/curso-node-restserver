61b75352ab19f9d791995b1b6190eee08e916502
Hugo-gmail <hazerdan@gmail.com>
2022-05-18 11:02:20 -0300

    Agregado de Variable

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server{
         this.app = express();
         this.port=process.env.PORT;
         this.usuariosPath = '/api/usuarios';
+        this.variable = process.env.VARIABLE;
 
         //Middlewares
         this.middlewares();
